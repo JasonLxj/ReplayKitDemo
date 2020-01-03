7d6f99ab84719fd5f5df405a7343eee7b4729a91
J Li <xiaojieli@6tiantian.com>
2020-01-03 12:26:05 +0800

    Initial Commit

---
+++ ReplayKit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ReplayKit
+//
+//  Created by ttlx on 2020/1/3.
+//  Copyright © 2020 ttlx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
